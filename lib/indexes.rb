65f194c254350176dddfee0835e1791cd02d47e9
pezholio <pezholio@gmail.com>
2014-11-18 09:01:10 +0000

    Add indexes to be created

--- lib/indexes.rb
+++ lib/indexes.rb
@@ -1,9 +1,11 @@
 module Distiller
   class Indexes
     def self.create
+      Address.create_indexes
       Street.create_indexes
       Postcode.create_indexes
       Locality.create_indexes
+      Town.create_indexes
     end
   end
 end
