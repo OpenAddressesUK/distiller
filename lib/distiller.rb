3ee2f08d2378c285151c270c7622493f370af647
pezholio <pezholio@gmail.com>
2014-11-21 15:26:09 +0000

    Load Dotenv earlier

--- lib/distiller.rb
+++ lib/distiller.rb
@@ -1,6 +1,10 @@
 require 'bundler'
 Bundler.require(:default)
 
+require 'dotenv'
+
+Dotenv.load
+
 require 'mongoid_address_models/require_all'
 
 Mongoid.load!(File.join(File.dirname(__FILE__), "..", "config", "mongoid.yml"), ENV["MONGOID_ENVIRONMENT"] || :development)
@@ -9,9 +13,6 @@ require 'httparty'
 require 'nokogiri'
 require 'zip'
 require 'uk_postcode'
-require 'dotenv'
-
-Dotenv.load
 
 require 'helpers'
 require 'import'
