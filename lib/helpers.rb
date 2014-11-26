f39e65def5bf94505e42b2dd387313450528c1b0
pezholio <pezholio@gmail.com>
2014-11-26 15:22:43 +0000

    Get SHA using Heroku API

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -10,7 +10,15 @@ module Distiller
     end
 
     def current_sha
-      @current_sha ||= `git rev-parse HEAD`.strip
+      if ENV['MONGOID_ENVIRONMENT'] == "production"
+        @current_sha ||= begin
+          heroku = PlatformAPI.connect_oauth(ENV['HEROKU_TOKEN'])
+          slug_id = heroku.release.list(ENV['HEROKU_APP']).last["slug"]["id"]
+          heroku.slug.info(ENV['HEROKU_APP'], slug_id)["commit"]
+        end
+      else
+        @current_sha ||= `git rev-parse HEAD`.strip
+      end
     end
 
   end
